71c4c2ff8e61b1b0632161de0598504251812ba1
Ashley Graham <charlotte.ashley.graham@gmail.com>
2022-02-23 11:23:32 -0600

    model

--- 03_Model.R
+++ 03_Model.R
@@ -20,16 +20,8 @@ candidate_vars <- c("loan_amount",
 df_HMDA_filtered <- df_HMDA %>%
   dplyr::select(approved,all_of(candidate_vars))
 
-model <- glm(approved ~ loan_amount + interest_rate_num + income_num + LTV_bin_80, data=df_HMDA_filtered)
+model <- glm(approved ~ property_value_num+income_num+LTV_bin_80+LTV_bin_90+LTV_bin_100+LTV_bin_gte100, 
+             data=df_HMDA_filtered,
+             family="binomial")
 df_HMDA_filtered$preds <- predict(model,df_HMDA_filtered)
-pROC::auc(df_HMDA_filtered$approved,df_HMDA_filtered$preds)
-
-model <- glm(approved ~ property_value_num+income_num+LTV_bin_80+LTV_bin_90+LTV_bin_100+LTV_bin_gte100, data=df_HMDA_filtered)
-df_HMDA_filtered$preds <- predict(model,df_HMDA_filtered)
-pROC::auc(df_HMDA_filtered$approved,df_HMDA_filtered$preds)
-
-full.model <- glm(approved ~ ., data=df_HMDA_filtered)
-step.model <- stepAIC(full.model, direction = "backward", 
-                      trace = FALSE)
-df_HMDA_filtered$preds <- predict(step.model,df_HMDA_filtered)
 pROC::auc(df_HMDA_filtered$approved,df_HMDA_filtered$preds)
